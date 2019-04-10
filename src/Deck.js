5a2e713dd0bef3a2da26785f1ba4b2285292fc4a
JohnnyC <j0hnnywchiang@gmail.com>
2019-04-10 02:24:41 -0700

    new images setup

--- src/Deck.js
+++ src/Deck.js
@@ -1,3 +1,24 @@
 export default function initializeDeck() {
   let id = 0;
+  const cards = [
+    "Aoi",
+    "Celeste",
+    "Chihiro",
+    "Junko",
+    "Kyoko",
+    "Maizono",
+    "Makoto",
+    "Toko"
+  ].reduce((acc, type) => {
+    acc.push({
+      id: id++,
+      type
+    });
+    acc.push({
+      id: id++,
+      type
+    });
+    return acc;
+  }, []);
+  return cards;
 }
