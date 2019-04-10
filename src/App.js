bbaaa36f593672ce77034146dbc7e81d2c726089
JohnnyC <j0hnnywchiang@gmail.com>
2019-04-10 01:15:46 -0700

    useState with getter method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Card from "./components/Card";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Memory Game</h1>
-        <h2>Can you remember where the cards are?</h2>
-        {/* <Card
-          id={1}
-          width={100}
-          height={100}
-          back={`/img/back.png`}
-          front={`/img/react.png`}
-          flipped={flipped.includes(1)}
-          handleClick={() => handleClick(1)}
-        /> */}
-      </div>
-    );
-  }
+function App() {
+  const [flipped, setFlipped] = useState([]);
+  const handleClick = id => setFlipped(id);
+
+  return (
+    <div>
+      <h1>Memory Game</h1>
+      <h2>Can you remember where the cards are?</h2>
+      <Card
+        id={1}
+        width={100}
+        height={100}
+        back={`/img/back.png`}
+        front={`/img/react.png`}
+        flipped={flipped.includes(1)}
+        handleClick={() => handleClick(1)}
+      />
+    </div>
+  );
 }
 
 export default App;
