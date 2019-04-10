38d49724032061f7a100861ba6b4660338bbb48d
JohnnyC <j0hnnywchiang@gmail.com>
2019-04-10 01:10:38 -0700

    card component remade

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,8 +1,39 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./Card.css";
 
-const Card = props => {
-  return null;
-};
+export default function Card({
+  handleClick,
+  id,
+  flipped,
+  back,
+  front,
+  height,
+  width
+}) {
+  return (
+    <div
+      className={`flip-container ${flipped ? "flipped" : ""}`}
+      style={{ width, height }}
+      onClick={() => handleClick(id)}
+    >
+      <div className="flipper">
+        <img
+          style={{ height, width }}
+          className={flipped ? "front" : "back"}
+          src={flipped ? front : back}
+        />
+      </div>
+    </div>
+  );
+}
 
-export default Card;
+Card.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
+  flipped: PropTypes.bool.isRequired,
+  back: PropTypes.string.isRequired,
+  front: PropTypes.string.isRequired,
+  height: PropTypes.number.isRequired,
+  width: PropTypes.number.isRequired
+};
