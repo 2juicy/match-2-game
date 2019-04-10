38d1979f3434576ed20b0f571009ac98010005ab
JohnnyC <j0hnnywchiang@gmail.com>
2019-04-10 01:51:36 -0700

    Updates

---
+++ precache-manifest.2f34c8c14292efc6211de2d68a4a8d9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44ec936a5a3750211662",
+    "url": "/match-2-game/static/js/runtime~main.47765068.js"
+  },
+  {
+    "revision": "dde94a7629677d43bbed",
+    "url": "/match-2-game/static/js/main.84168c82.chunk.js"
+  },
+  {
+    "revision": "76e473e679b1c9bd19b3",
+    "url": "/match-2-game/static/js/2.67e49a41.chunk.js"
+  },
+  {
+    "revision": "dde94a7629677d43bbed",
+    "url": "/match-2-game/static/css/main.09c7e7b1.chunk.css"
+  },
+  {
+    "revision": "8dd53bf510b4bd7edf0413acd8462921",
+    "url": "/match-2-game/index.html"
+  }
+];
