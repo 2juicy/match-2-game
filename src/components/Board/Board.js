764e947816eb00b1d9cf8a1c764d14871b4875f7
JohnnyC <j0hnnywchiang@gmail.com>
2019-04-10 02:07:08 -0700

    board maping cards

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,24 +1,29 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import Card from '../Card'
+import PropTypes from "prop-types";
+import Card from "../Card";
 
-function Board({ cards, flipped, handleClick}){
-    return <div className="board">
-    { cards.map(card => 
-    <Card
-        id={card.id}
-        type={card.type}
-        width={card.width}
-        height={card.height}
-        flipped={flipped.includes(card.id)}
-        handleClick={() => handleClick(card.id)}
-      />)}
-
+function Board({ cards, flipped, handleClick }) {
+  return (
+    <div className="board">
+      {cards.map(card => (
+        <Card
+          key={card.id}
+          id={card.id}
+          type={card.type}
+          width={card.width}
+          height={card.height}
+          flipped={flipped.includes(card.id)}
+          handleClick={() => handleClick(card.id)}
+        />
+      ))}
     </div>
+  );
 }
 
-Card.propTypes = {
-    cards.
-}
+Board.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  flipped: PropTypes.arrayOf(PropTypes.numbers).isRequired,
+  handleClick: PropTypes.func.isRequired
+};
 
 export default Board;
