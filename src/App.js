8b3a67c49683b56bf875ac0cb408fc4b63719644
JohnnyC <j0hnnywchiang@gmail.com>
2019-04-10 00:39:36 -0700

    initial card setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import Card from "./components/card";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Nothing in here for now, initial setup</h1>
+        <h1>Memory Game</h1>
+        <h2>Can you remember where the cards are?</h2>
+        <Card
+          id={1}
+          width={100}
+          height={100}
+          back={`/img/back.png`}
+          front={`/img/react.png`}
+          flipped={flipped.includes(1)}
+          handleClick={() => handleClick(1)}
+        />
       </div>
     );
   }
